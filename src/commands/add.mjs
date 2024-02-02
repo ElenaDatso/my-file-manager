792ec9a7b9df86661c805c5f5f2862f290e5efff
Olena Datso <elena.shcheglova@gmail.com>
2024-02-01 17:48:30 -0700

    feat: add create command

---
+++ src/commands/add.mjs
@@ -0,0 +1,15 @@
+import fs from 'fs';
+import path from 'path';
+
+
+export const addCommand = (fileName) => {
+  const currentWD = process.cwd();
+  const filePath = path.join(currentWD, fileName);
+  fs.writeFile(filePath, '', (err) => {
+  if (err) {
+    console.error('Error creating the file:', err);
+  } else {
+    console.log(`Success! The file is created \/n You are currently in ${currentWD}`);
+  }
+});
+}
