257ea4fc016537edd8f3a8d88174eba5e2967729
Olena Datso <elena.shcheglova@gmail.com>
2024-02-02 18:37:41 -0700

    refactor: add new funcs to index

--- src/commands/index.mjs
+++ src/commands/index.mjs
@@ -1,11 +1,21 @@
 import { closeEvent } from "./basic/close.mjs";
-import { upCommand } from "./basic/up.mjs";
+import { upCommand } from "./navigating/up.mjs";
 import { addCommand } from "./basic/add.mjs";
 import { deleteCommand } from './basic/delete.mjs';
+import { cdCommand } from "./navigating/cd.mjs";
+import { listCommand } from "./navigating/list.mjs";
+import { catCommand } from "./basic/cat.mjs";
+import { renameCommand } from "./basic/rename.mjs";
+import { copyCommand } from "./basic/copy.mjs";
 
 export const commandList = {
   close: closeEvent,
   up: upCommand,
   add: addCommand,
   delete: deleteCommand,
+  cd: cdCommand,
+  list: listCommand,
+  cat: catCommand,
+  rn: renameCommand,
+  cp: copyCommand,
 }
