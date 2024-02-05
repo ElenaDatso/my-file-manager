51610cf757e15c8859bc1e82810972198fb31a07
Olena Datso <elena.shcheglova@gmail.com>
2024-02-05 14:33:29 -0700

    refactor: change msg

--- src/commands/basic/add.mjs
+++ src/commands/basic/add.mjs
@@ -1,5 +1,6 @@
 import fs from 'fs';
 import path from 'path';
+import { msg } from '../../messages/msg.mjs';
 
 
 export const addCommand = (fileName) => {
@@ -7,7 +8,7 @@ export const addCommand = (fileName) => {
   const filePath = path.join(currentWD, fileName);
   fs.writeFile(filePath, '', (err) => {
   if (err) {
-    console.error('Error creating the file:', err);
+    msg.opFailed();
   } else {
     console.log(`Success! The file is created /n You are currently in ${currentWD}`);
   }
