c93f5a3a98cd2d9989225fc4e22471d170f1e98e
Olena Datso <elena.shcheglova@gmail.com>
2024-02-05 15:50:39 -0700

    fix: compress func

--- src/commands/compress/compress.mjs
+++ src/commands/compress/compress.mjs
@@ -1,22 +1,53 @@
-import { createReadStream, createWriteStream } from 'fs';
+import { createReadStream, createWriteStream, existsSync, statSync } from 'fs';
 import { createBrotliCompress } from 'zlib';
 import { pipeline } from 'stream';
 import { promisify } from 'util';
+import path from 'path';
+import { msg } from '../../messages/msg.mjs';
 
 const pipelineAsync = promisify(pipeline);
 
-export async function compressCommand (pathToFile, pathToDestination) {
-  const source = createReadStream(pathToFile);
-  const destination = createWriteStream(pathToDestination);
+export async function compressCommand(pathToFile, pathToDestination) {
+  // Определяем абсолютные пути
+  const absolutePathToFile = path.resolve(pathToFile);
+  let absolutePathToDestination = path.resolve(pathToDestination);
+
+  if (
+    existsSync(absolutePathToDestination) &&
+    statSync(absolutePathToDestination).isDirectory()
+  ) {
+    const fileName =
+      path.basename(pathToFile, path.extname(pathToFile)) + '.br';
+    absolutePathToDestination = path.join(absolutePathToDestination, fileName);
+  } else {
+
+    const destinationDir = path.dirname(absolutePathToDestination);
+    if (!existsSync(destinationDir)) {
+      throw new Error(
+        `Destination directory does not exist: ${destinationDir}`
+      );
+    }
+    if (
+      path.basename(absolutePathToDestination) ===
+      path.dirname(absolutePathToDestination)
+    ) {
+      const fileName =
+        path.basename(pathToFile, path.extname(pathToFile)) + '.br';
+      absolutePathToDestination = path.join(destinationDir, fileName);
+    }
+  }
+
+  const source = createReadStream(absolutePathToFile);
+  const destination = createWriteStream(absolutePathToDestination);
   const brotliCompress = createBrotliCompress();
 
   try {
     await pipelineAsync(source, brotliCompress, destination);
     console.log(
-      'File successfully compressed using Brotli and written to destination.'
+      'File successfully compressed using Brotli and written to destination:',
+      absolutePathToDestination
     );
-  } catch (err) {
-    console.error('An error occurred:', err);
+  } catch {
+    msg.opFailed();
   }
-};
-
+}
