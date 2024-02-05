2ebc419b72dd11d0f66cdb1a7a19e0766c504e45
Olena Datso <elena.shcheglova@gmail.com>
2024-02-05 15:56:06 -0700

    refactor

--- src/commands/basic/delete.mjs
+++ src/commands/basic/delete.mjs
@@ -1,9 +1,16 @@
 import fs from 'fs';
+import { msg } from '../../messages/msg.mjs';
 
 export const deleteCommand = (pathToFile, curWD) => {
-  fs.rm(pathToFile, () => {
-    console.log(
-      `The file was succsfully removed. /n You are currently in ${curWD}}`
-    );
-  });
+  try {
+    fs.rm(pathToFile, () => {
+      console.log(
+        `The file was succsfully removed. /n You are currently in ${curWD}}`
+      );
+    });
+    msg.curDirMsg(process.cwd());
+  } catch {
+    msg.opFailed();
+  }
+
 };
